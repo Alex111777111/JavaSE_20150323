e8797fc756dc191acf2b63c028e39b9a7812c009
sbt-saburov-ag <sbt-saburov-ag@sberbank.ru>
2015-06-11 21:34:17 -0400

    Part 19.1

--- BookCatalogWeb/src/ru/javacourse/book/web/BookEditServlet.java
+++ BookCatalogWeb/src/ru/javacourse/book/web/BookEditServlet.java
@@ -13,19 +13,29 @@ import java.io.IOException;
 /**
  * Created by Антон on 04.06.2015.
  */
-public class BookEditServlet extends HttpServlet
-{
+public class BookEditServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String bookIdStr = request.getParameter("bookId");
         String title = request.getParameter("title");
         String priceStr = request.getParameter("price");
         Double price = Double.parseDouble(priceStr);
         String isbn = request.getParameter("isbn");
 
-        Book book = new Book(null, title, price, isbn);
-        try {
-            BookCatalog.getInstance().addBook(book);
-        } catch (BookBusinessException e) {
-            e.printStackTrace();
+        if (bookIdStr != null && !bookIdStr.trim().isEmpty()) {
+            try {
+                Long bookId = Long.parseLong(bookIdStr);
+                Book book = new Book(bookId, title, price, isbn);
+                BookCatalog.getInstance().updateBook(book);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            Book book = new Book(null, title, price, isbn);
+            try {
+                BookCatalog.getInstance().addBook(book);
+            } catch (BookBusinessException e) {
+                e.printStackTrace();
+            }
         }
         response.sendRedirect("bookList");
     }
